efd6c3aaf2917f8a8bd2c44f3cc93ccbbed2aff1
A2L5E6 <44853490+A2L5E6@users.noreply.github.com>
2019-12-01 22:39:49 -0500

    Update SampleCracker.py

--- crackers/SampleCracker.py
+++ crackers/SampleCracker.py
@@ -1,22 +1,40 @@
+
 import math
-targets=['brumalia']
+import time
+from tqdm import tqdm
+print('Sample cracker: Science Fair 2019/2020 Aiden Lee')
+targets=['Counselling',' epiperipheral','semichorus','preexpectation','secondsighted','pasgarde','tuberculum','brumalia','songy','barrancas']
+times=[0,0,0,0,0,0,0,0,0,0]
+sums=[0,0,0,0,0,0,0,0,0,0]
 filepath = r'C:\Users\MacWin\Desktop\english-words-master\english-words-master\words_alpha.txt'
-cracked=0
 line =''
 cnt = 1
-def cipher(x):
-    return x
-with open(filepath) as fp:
-   line = fp.readline()
-   cnt = 1
-   while line:
-       per=math.floor((cnt/370103)*100000)/1000
-       print("Testing line {} of 370103 ({}%): {}".format(cnt,per, line.strip()))
-       for i in targets:
-            if cipher(line.strip()) == "brumalia":
-             cracked=cnt
-             print('found')
-             break
-       line=fp.readline()
-       cnt += 1
-print("found on line:{}".format(cracked))
+trial=0
+trialsToDo=5
+print('Scheduled for {} trials'.format(trialsToDo))
+for g in range(trialsToDo):
+    print('\nTrial {} started'.format(g+1))
+    fbidn=tqdm(total=370103,leave=True)
+    with open(filepath) as fp:
+        startTime=time.time()
+        line=''
+        line = fp.readline()
+        cnt = 1
+        while line:
+           ciphertext=line.strip()
+           #print("Testing line {} of 370103 ({}%): {} to {}".format(cnt,per,line.strip(),ciphertext)) 
+           fbidn.update(1)  
+           line=fp.readline()
+           cnt += 1
+           tmp=0
+           for i in targets:
+               if i == ciphertext:
+                   times[tmp]=time.time()-startTime
+                   sums[tmp]+=time.time()-startTime
+               tmp+=1
+    fbidn.close()
+    print('Time taken: {} seconds'.format(time.time()-startTime))
+    print('times:{}'.format(times))
+print('avar times')
+for i in sums:
+    print(i/trialsToDo)
