f45e4631f291f047a81ca06948483ff9a0015968
A2L5E6 <44853490+A2L5E6@users.noreply.github.com>
2019-12-01 22:36:56 -0500

    Update and rename crackers/ScifairEnigma.py to crackers/Enigma/ScifairEnigma.py

--- crackers/Enigma/ScifairEnigma.py
+++ crackers/Enigma/ScifairEnigma.py
@@ -3,14 +3,15 @@ import math
 from enigma.machine import EnigmaMachine
 import time
 from tqdm import tqdm
-
+print('Enigma Machine: Science Fair 2019/2020 Aiden Lee')
 targets=['DEDGEGNVLPO','AWXWSFUDNSWXI','YOADZZMTEE','GMBOHTJHYLJNZX','YOKEQLYWRDJVJ','GGQNBFQY','NDEOFJIVEB','KMDRBDUZ','YEONV','KGWMBXTZX']
-times=[]
+times=[0,0,0,0,0,0,0,0,0,0]
+sums=[0,0,0,0,0,0,0,0,0,0]
 filepath = r'C:\Users\MacWin\Desktop\english-words-master\english-words-master\words_alpha.txt'
 line =''
 cnt = 1
 trial=0
-
+trialsToDo=5
 machine = EnigmaMachine.from_key_sheet(
        rotors='II IV V',
        reflector='B',
@@ -19,13 +20,12 @@ machine = EnigmaMachine.from_key_sheet(
 machine.set_display('WXC')
 msg_key = machine.process_text('KCH')
 machine.set_display(msg_key)
-print('Enigma Machine: Science Fair 2019/2020 Aiden Lee')
-for g in range(10):
+print('Scheduled for {} trials'.format(trialsToDo))
+for g in range(trialsToDo):
     print('\nTrial {} started'.format(g+1))
     fbidn=tqdm(total=370103,leave=True)
     with open(filepath) as fp:
         startTime=time.time()
-        times.insert(trial,[])
         line=''
         line = fp.readline()
         cnt = 1
@@ -41,7 +41,12 @@ for g in range(10):
            tmp=0
            for i in targets:
                if i == ciphertext:
-                   times[trial].insert(tmp,time.time()-startTime)
+                   times[tmp]=time.time()-startTime
+                   sums[tmp]+=time.time()-startTime
                tmp+=1
     fbidn.close()
     print('Time taken: {} seconds'.format(time.time()-startTime))
+    print('times:{}'.format(times))
+print('avar times')
+for i in sums:
+    print(i/trialsToDo)
